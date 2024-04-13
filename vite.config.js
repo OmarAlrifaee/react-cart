a47554206454c9e3b7ed2470eb36ec1a746c6e6b
OmarAlrifaee <jivara211@gmail.com>
2024-04-13 16:12:48 +0300

    edited

--- vite.config.js
+++ vite.config.js
@@ -2,4 +2,4 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
-export default defineConfig({ base: "/{react-cart}", plugins: [react()] });
+export default defineConfig({ base: "/react-cart", plugins: [react()] });
